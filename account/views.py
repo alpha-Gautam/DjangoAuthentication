35baa7eb71b4728daefe0259407869d4726ba836
Gautam Maurya <gautammaurya1299@gmail.com>
2025-07-01 13:06:28 +0530

    Refactor settings to use environment variable for SECRET_KEY and implement login functionality in LoginView

--- account/views.py
+++ account/views.py
@@ -49,3 +49,17 @@ class ActivateView(APIView):
             return Response({"message": "Account activated successfully."}, status=status.HTTP_200_OK)
         else:
             return Response({"error": "Activation link is invalid or has expired."}, status=status.HTTP_400_BAD_REQUEST)
+        
+     
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    @method_decorator(ensure_csrf_cookie)
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({"message": "Login successful."}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid credentials."}, status=status.HTTP_400_BAD_REQUEST)   
--- accountauthentication/settings.py
+++ accountauthentication/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1604p26klwmy^kwwx-xw2&m10@_^0wtm^7=eh3v@@!_@ls3i)o'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
